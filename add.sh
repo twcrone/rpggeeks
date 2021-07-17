69ffc02a26cd761c402e6448d6afb6fafcc453c6
Todd Crone <twcrone@gmail.com>
2021-07-17 16:17:32 -0400

    Add shell script for adding a couple players

---
+++ add.sh
@@ -0,0 +1,3 @@
+curl https://rpggeeks.herokuapp.com/create\?name\=Todd\&email\=twcrone@gmail.com
+curl https://rpggeeks.herokuapp.com/create\?name\=Matt\&email\=eightbit@gmail.com
+curl https://rpggeeks.herokuapp.com/list
